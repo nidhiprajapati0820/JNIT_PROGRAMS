859cf72c8a1c96e27d1012b7525be8d3d94f3641
nidhiprajapati0820 <nidhi.khurpia@gmail.com>
2021-10-16 11:53:21 -0700

    updated

--- src/assignmentsJNIT/abstraction/AbstractDemo1.java
+++ src/assignmentsJNIT/abstraction/AbstractDemo1.java
@@ -16,7 +16,7 @@ class Audi extends Car {
 	}
 
 	void mileage() {
-		System.out.println("Audi mileage is 15mpg");
+		System.out.println("Audi mileage is 15 miles per gallon");
 	}
 	
 }
@@ -28,7 +28,7 @@ class BMW extends Car {
 	}
 
 	void mileage() {
-		System.out.println("BMW mileage is 13mpg");
+		System.out.println("BMW mileage is 13 miles per gallon");
 	}
 	
 }
