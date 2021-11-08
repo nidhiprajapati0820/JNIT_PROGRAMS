02494956ff8d743fb393a8ef0cb6727de2a33f0f
nidhiprajapati0820 <nidhi.khurpia@gmail.com>
2021-11-07 23:20:02 -0700

    updated

--- src/assignmentsJNIT/ExceptionHandling/Arithmetic_Exception.java
+++ src/assignmentsJNIT/ExceptionHandling/Arithmetic_Exception.java
@@ -14,7 +14,7 @@ public class Arithmetic_Exception {
 			a = 65 / b;
 			System.out.println(a);
 			System.out.println("Hey I'm at the end of try block");
-			c= arr[0];
+			c= arr[4];
 
 		} catch (ArrayIndexOutOfBoundsException e) {
 			System.out.println("Array out of range");
@@ -27,7 +27,7 @@ public class Arithmetic_Exception {
 		}
 		
 		finally {
-			System.out.println("I always execute ");
+			System.out.println("I always get executed ");
 		}
 
 	}
